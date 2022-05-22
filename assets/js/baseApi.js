e7a3a31e96e18effdc06d1583c3841ed7282416e
haixing233 <1600570131@qq.com>
2022-05-22 23:11:30 +0800

    完成了主页

--- assets/js/baseApi.js
+++ assets/js/baseApi.js
@@ -1,3 +1,21 @@
 $.ajaxPrefilter(function(options) {
     options.url = 'http://www.liulongbin.top:3007' + options.url
+
+
+    // 给所有url里带着my路径的请求 添加请求头
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+
+
+    //全局挂载一个complete验证
+    options.complete = function(res) {
+        if (res.responseJSON.status === 1 && res.responseJSON.message === "身份认证失败！") {
+            localStorage.removeItem('token')
+            location.href = '/login.html'
+        }
+
+    }
 })
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,25 +1,55 @@
 $(function() {
-        getUserInfo()
+    getUserInfo()
+
+    let layer = layui.layer
+    $('#btn-exit').on('click', function() {
+
+        layer.confirm('是否退出?', { icon: 3, title: '提示' }, function(index) {
+            //do something
+            localStorage.removeItem('token')
+            location.href = '/login.html'
+
+            //执行结束关闭弹窗
+            layer.close(index);
+        });
+
     })
-    //获取用户的基本信息
+
+})
+
+
+
+
+
+//获取用户的基本信息
 function getUserInfo() {
     $.ajax({
             method: 'GET',
             url: '/my/userinfo',
+            async: false,
             //请求头配置
-            headers: {
-                Authorization: localStorage.getItem('token')
-            },
+            // headers: {
+            //     Authorization: localStorage.getItem('token')
+            // },
             success: function(res) {
-                renderAvatar(res.data)
+                    if (res.status === 0)
+                        renderAvatar(res.data)
 
-            }
+                }
+                // complete: function(res) {
+                //     if (res.responseJSON.status === 1 && res.responseJSON.message === "身份认证失败！") {
+                //         localStorage.removeItem('token')
+                //         location.href = '/login.html'
+                //     }
+
+            // }
         }
 
     )
 
     function renderAvatar(user) {
-        let name = user.nicknam || user.username
+
+        let name = user.nickname || user.username
 
         $('#welcome').html('欢迎&nbsp; &nbsp;' + name)
         $('#pushname').html(name)
