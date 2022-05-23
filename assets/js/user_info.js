508da46d8903350c29b7ee57eb61dfa64692e359
Haixing <1600570131@qq.com>
2022-05-23 09:53:53 +0800

    完成了修改密码页面的渲染

--- assets/js/user_info.js
+++ assets/js/user_info.js
@@ -2,13 +2,15 @@ $(function() {
     let form = layui.form
     let layer = layui.layer
 
-    form.verifl({
+    form.verify({
         nikcname: function(value) {
             if (value.lenght > 6)
                 return '昵称长度必须要1-6个字符之间'
         }
 
+
     })
+
 
     iniyUserInfo()
 
@@ -21,7 +23,8 @@ $(function() {
                 if (res.status !== 0) {
                     return layer.msg("请求用户信息失败！")
                 }
-
+                //调用form val 对表单进行赋值
+                form.val('formUserInfo', res.data)
 
             }
 
@@ -29,4 +32,28 @@ $(function() {
 
     }
 
+    $('#btnReset').on('click', function(e) {
+        //阻止默认提交
+        e.preventDefault()
+        iniyUserInfo()
+
+    })
+
+    $('.layui-form').on('submit', function(e) {
+        e.preventDefault()
+
+        $.ajax({
+            method: 'POST',
+            url: '/my/userinfo',
+            data: $(this).serialize(),
+            success: function(res) {
+                if (res.status !== 0) {
+                    return layer.msg('数据提交失败！')
+                }
+                layer.msg('数据提交成功！')
+                    //从父窗口调用getUserInfo 重新渲染页面
+                window.parent.getUserInfo()
+            }
+        })
+    })
 })
